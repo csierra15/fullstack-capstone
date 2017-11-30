09aeedee1054f9b198651aa7bab9fa9756465b5b
Carly Sierra <carlylwomack@gmail.com>
2017-11-30 12:00:12 -0600

    added addt'l html pages & tests

--- test/test-server.js
+++ test/test-server.js
@@ -7,7 +7,43 @@ const {app} = require('../server');
 chai.use(chaiHttp);
 
 describe('index page', function() {
-    it('is real', function(done) {
+    it('exists', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
+
+describe('departments page', function() {
+    it('exists', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
+
+describe('lists page', function() {
+    it('exists', function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
+
+describe('signup page', function() {
+    it('exists', function(done) {
         chai.request(app)
             .get('/')
             .end(function(err, res) {
