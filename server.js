608e2eb98b6b98612520170d38126ebbbcd2c2eb
Carly Sierra <carlylwomack@gmail.com>
2017-11-30 10:43:35 -0600

    added endpoint

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.json({'message': 'answer'});
+})
 app.listen(process.env.PORT || 8080);
 
 module.exports = {app};
